1c8f07bc113bf6cdda2414a2ef7fdc4e9e5a5615
workes <bassdard2012@gmail.com>
2026-04-21 01:42:15 +0200

    Update Thunderstore package

--- obj/Debug/InteractableCreditMultiplier.AssemblyInfo.cs
+++ obj/Debug/InteractableCreditMultiplier.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InteractableCreditMultiplier")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5e72ab260a665c2ab43fca87d82e67d403e4d40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77721f11b444a600a812130dd42fcd5d53cdb0fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("InteractableCreditMultiplier")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InteractableCreditMultiplier")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
